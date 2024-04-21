86c458693f93c98f5f26c955f5f4e0c1dbc893f4
hj <kwonfelix93@gmail.com>
2024-04-21 18:38:03 +0900

    refactor : @CreatedAt 어노테이션 삭제

--- src/main/java/com/zerobase/fintech/transaction/entity/WithdrawForm.java
+++ src/main/java/com/zerobase/fintech/transaction/entity/WithdrawForm.java
@@ -1,11 +1,9 @@
 package com.zerobase.fintech.transaction.entity;
 
 import com.zerobase.fintech.account.entity.AccountEntity;
-import jakarta.persistence.PrePersist;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -31,17 +29,7 @@ public class WithdrawForm {
   private int balance;
   private String transactionName;
   private boolean verify;
-
-
   private LocalDateTime createAt;
-
-  @PrePersist
-  public void onPrePersist() {
-    String customLocalDateTimeFormat = LocalDateTime.now().format(
-        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-    this.createAt = LocalDateTime.parse(customLocalDateTimeFormat,
-        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-  }
 
   public static TransactionEntity toEntity(WithdrawForm request,
       AccountEntity accountEntity) {
